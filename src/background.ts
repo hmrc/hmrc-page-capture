33105864f0c2773f57aafbac8f8e57630986c91b
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2022-12-15 14:50:12 +0000

    PLATUI-2034: Add JavaScript based page capture

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,23 @@
-function saveUrlToStorage(url: string) {
+chrome.runtime.onMessage.addListener((request, sender, response) => {
+  // @ts-ignore
+  const documentId = sender.documentId;
+  const pageUrl = sender.url;
+  const page = request.payload;
+
+  savePageUrl(pageUrl);
+  getPageData(documentId, page, pageUrl);
+
+  page.forEach((asset: string) => {
+    const filename = JSON.parse(asset).filename;
+    const url = JSON.parse(asset).url;
+
+    downloadPage(documentId, filename, url);
+  });
+});
+
+function savePageUrl(url: string) {
   chrome.storage.sync.get({urls: []}, function (result) {
-    var urls = result.urls;
+    let urls = result.urls;
 
     urls.push(url);
     chrome.storage.sync.set({urls: urls}, function () {
@@ -9,24 +26,21 @@ function saveUrlToStorage(url: string) {
   });
 }
 
-function savePageToDownloads(documentId: String, tabId: number) {
-  chrome.pageCapture.saveAsMHTML({tabId: tabId}, async (blob) => {
-    const content = await blob.text();
-    const url = 'data:application/x-mimearchive;base64,' + btoa(content);
-
-    chrome.downloads.download({
-      url,
-      filename: `${documentId}/index.mhtml`,
-    });
+function getPageData(documentId: string, page: string[], url: string) {
+  const content = btoa(JSON.stringify({documentId: documentId, url: url}));
+  const payload = JSON.stringify({
+    url: `data:application/json;base64,${content}`,
+    filename: 'data.json',
   });
+
+  page.push(payload);
+
+  return page;
 }
 
-chrome.runtime.onMessage.addListener((request, sender, response) => {
-  // @ts-ignore
-  const documentId = sender.documentId;
-  const url = sender.url;
-  const tabId = sender.tab.id;
-
-  saveUrlToStorage(url);
-  savePageToDownloads(documentId, tabId);
-});
+function downloadPage(documentId: string, filename: string, url: string) {
+  chrome.downloads.download({
+    url,
+    filename: `${documentId}/${filename}`,
+  });
+}
--- src/content.ts
+++ src/content.ts
@@ -1,7 +1,36 @@
 window.onload = () => {
-  sendMessage();
+  let page = getPage();
+
+  sendMessage(page);
 };
 
-function sendMessage() {
-  chrome.runtime.sendMessage({payload: 'Capture URL and page source.'});
+function sendMessage(page: string[]) {
+  chrome.runtime.sendMessage({
+    payload: page,
+  });
+}
+
+function getPage() {
+  let page: string[] = [];
+  const source = document.cloneNode(true) as Document;
+  const selector = source.head.querySelectorAll('link[rel=stylesheet]');
+
+  selector.forEach((stylesheet: HTMLLinkElement) => {
+    const href = stylesheet.href;
+    const filename = href.split('/').pop();
+    const payload = JSON.stringify({url: href, filename: filename});
+
+    page.push(payload);
+    stylesheet.setAttribute('href', `./${filename}`);
+  });
+
+  const content = btoa(source.documentElement.outerHTML);
+  const payload = JSON.stringify({
+    url: `data:text/html;base64,${content}`,
+    filename: 'index.html',
+  });
+
+  page.push(payload);
+
+  return page;
 }
